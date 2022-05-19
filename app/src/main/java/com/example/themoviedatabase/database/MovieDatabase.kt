a7afc116cb417c47c74295e9ed79191546e59d30
Tilen Prepeluh <tpr@shpock.com>
2022-05-19 18:24:39 +0200

    add database

---
+++ app/src/main/java/com/example/themoviedatabase/database/MovieDatabase.kt
@@ -0,0 +1,41 @@
+package com.example.themoviedatabase.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.themoviedatabase.database.dao.ImagePathDao
+import com.example.themoviedatabase.database.dao.MovieDao
+import com.example.themoviedatabase.database.dao.MoviePagingKeysDao
+import com.example.themoviedatabase.database.table.ImagePathTable
+import com.example.themoviedatabase.database.table.MoviePagingKeysTable
+import com.example.themoviedatabase.database.table.MovieTable
+
+@Database(
+    entities = [
+        ImagePathTable::class,
+        MovieTable::class,
+        MoviePagingKeysTable::class
+    ], version = 1
+)
+abstract class MovieDatabase : RoomDatabase() {
+    abstract fun getImagePathDao(): ImagePathDao
+    abstract fun getMovieDao(): MovieDao
+    abstract fun getMoviePagingKeysDao(): MoviePagingKeysDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: MovieDatabase? = null
+
+        fun getInstance(context: Context): MovieDatabase =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE
+                    ?: buildDatabase(context).also { INSTANCE = it }
+            }
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(context, MovieDatabase::class.java, "movie_database")
+                .build()
+    }
+}
