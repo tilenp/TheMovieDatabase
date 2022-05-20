94e1b81c5e72ce23d0ddf0eedb3cbee6b9265dc9
Tilen Prepeluh <tpr@shpock.com>
2022-05-20 16:14:28 +0200

    LoadingViewTest

---
+++ app/src/androidTest/java/com/example/themoviedatabase/ui/common/LoadingViewTest.kt
@@ -0,0 +1,39 @@
+package com.example.themoviedatabase.ui.common
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import com.example.themoviedatabase.ui.theme.TheMovieDatabaseTheme
+import org.junit.Rule
+import org.junit.Test
+
+class LoadingViewTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun visibility_test() {
+        // arrange
+        composeTestRule.setContent {
+            TheMovieDatabaseTheme {
+                Scaffold { padding ->
+                    LoadingView(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(padding))
+                }
+            }
+        }
+
+        // just so you can see it on the phone
+        Thread.sleep(1000)
+
+        // assert
+        composeTestRule.onNodeWithTag("LoadingIndicator").assertIsDisplayed()
+    }
+}
