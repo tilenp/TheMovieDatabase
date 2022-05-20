2a6d4c6af34a30090fed941505d353c445747f3b
Tilen Prepeluh <tpr@shpock.com>
2022-05-21 00:00:20 +0200

    UIDataTest

---
+++ app/src/androidTest/java/com/example/themoviedatabase/utils/UIDataTest.kt
@@ -0,0 +1,38 @@
+package com.example.themoviedatabase.utils
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.example.themoviedatabase.R
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class UIDataTest {
+
+    private val context: Context = ApplicationProvider.getApplicationContext()
+
+    @Test
+    fun string_test() {
+        // arrange
+        val string = "string"
+        val uiText = UIText(string = string)
+
+        // act
+        val result = uiText.asString(context)
+
+        // assert
+        assertEquals(string, result)
+    }
+
+    @Test
+    fun string_id_test() {
+        // arrange
+        val stringId = R.string.Nothing_to_show
+        val uiText = UIText(stringId = stringId)
+
+        // act
+        val result = uiText.asString(context)
+
+        // assert
+        assertEquals(context.getString(stringId), result)
+    }
+}
